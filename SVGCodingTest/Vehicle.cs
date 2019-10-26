61fbddfbdca3439e276baa3990d13c3aa3335703
Will McKinley <mckinleywc@gmail.com>
2019-10-26 19:35:54 -0400

    Initial Commit ASP/C# Web framework and Vehicle class.

---
+++ SVGCodingTest/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVGCodingTest
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+
+        public int QuantSold18 { get; set; }
+
+        public int QuantSold19 { get; set; }
+
+        public int TopYear()
+        {
+            var year = QuantSold18 > QuantSold19 ? 18 : 19;
+            return year;
+        }
+
+        public int QuantDiff()
+        {
+            var result = QuantSold18 > QuantSold19 ? QuantSold18 - QuantSold19 : QuantSold19 - QuantSold18;
+            return result;
+        }
+    }
+}
