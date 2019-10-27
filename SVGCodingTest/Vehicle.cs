84e2ac57debbee9aa59a804d782765fdc68d241a
Will McKinley <mckinleywc@gmail.com>
2019-10-26 20:26:43 -0400

    Commit2
    changes to vehicle class for abstraction, added vehicle methods for market share calculations

--- SVGCodingTest/Vehicle.cs
+++ SVGCodingTest/Vehicle.cs
@@ -9,19 +9,23 @@ namespace SVGCodingTest
     {
         public string Type { get; set; }
 
-        public int QuantSold18 { get; set; }
+        public int QuantSoldY1 { get; set; }
 
-        public int QuantSold19 { get; set; }
+        public int QuantSoldY2 { get; set; }
+
+        public float MarketY1 { get; set; }
+
+        public float MarketY2 { get; set; }
 
         public int TopYear()
         {
-            var year = QuantSold18 > QuantSold19 ? 18 : 19;
+            var year = QuantSoldY1 > QuantSoldY2 ? 1 : 2;
             return year;
         }
 
         public int QuantDiff()
         {
-            var result = QuantSold18 > QuantSold19 ? QuantSold18 - QuantSold19 : QuantSold19 - QuantSold18;
+            var result = QuantSoldY1 > QuantSoldY2 ? QuantSoldY1 - QuantSoldY2 : QuantSoldY2 - QuantSoldY1;
             return result;
         }
     }
---
+++ SVGCodingTest/VehicleMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVGCodingTest
+{
+    public class VehicleMethods
+    {
+        private int totalY1 = 0, totalY2 = 0;
+        private List<Vehicle> vehicles = new List<Vehicle>();
+        public List<Vehicle> MarketShares(List<Vehicle> vehicles)
+        {
+            this.vehicles = vehicles;
+            Totals();
+            Shares();
+            return this.vehicles;
+        }
+
+        private void Totals()
+        {
+            try
+            {
+                foreach (var vehicle in vehicles)
+                {
+                    totalY1 += vehicle.QuantSoldY1;
+                    totalY2 += vehicle.QuantSoldY2;
+                }
+            }
+            catch(NullReferenceException ex)
+            {
+                // to do error logger
+            }
+        }
+
+        private void Shares()
+        {
+            try
+            {
+                foreach (var vehicle in vehicles)
+                {
+                    vehicle.MarketY1 = vehicle.QuantSoldY1 / totalY1;
+                    vehicle.MarketY2 = vehicle.QuantSoldY2 / totalY2;
+                }
+            }
+            catch(NullReferenceException ex)
+            {
+                //to do error logger
+            }
+        }
+    }
+}
